d2c0e4e0d4d394332c357ed6f1609c09bdc873d9
VieziDev <matheusviezi@gmail.com>
2024-04-18 16:48:57 -0300

    adjusting vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/' // Substitua NOME_DO_REPOSITORIO pelo nome do seu repositório no GitHub.
+  base: '/ramdom-compliments/' // Substitua NOME_DO_REPOSITORIO pelo nome do seu repositório no GitHub.
 });
